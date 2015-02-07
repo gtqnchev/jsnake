9b9186d4d700936cbf0383921ba02f14b4ceefff
George <gtqnchev@gmail.com>
2015-02-07 23:20:38 +0200

    Handle collisions and add UI controls

--- snake.js
+++ snake.js
@@ -19,12 +19,27 @@ var Board = (function(){
         return matrix;
     }
 
-    function setMatrix(matrix, fill) {
+    function clearSnakes(matrix, fill) {
         for(i = 0; i < matrix.length; i++) {
             for(j = 0; j < matrix[i].length; j++) {
-                matrix[i][j] = fill;
+                if(matrix[i][j] === 1){
+                    matrix[i][j] = fill;
+                }
             }
         }
+    }
+
+    function findRandomFreeCell() {
+        var x = _.random(0, this.m - 1),
+            y = _.random(0, this.n - 1);
+        console.log(x,y, this.matrix);
+        // TODO: this will block if there are no free cells
+        while(this.matrix[x][y] != this.cell.free) {
+            x = (x + 1) % this.m;
+            y = (y + 1) % this.n;
+        }
+
+        return { x: x, y: y };
     }
 
     return {
@@ -58,12 +73,33 @@ var Board = (function(){
         },
 
         moveSnakes: function() {
-            _.each(this.snakes, function(snake) {
-                snake.move();
-            });
-
-            setMatrix(this.matrix, this.cell.free);
+            _.each(this.snakes, this.decideSnakeMove.bind(this));
+            clearSnakes(this.matrix, this.cell.free);
             this.markSnakes();
+        },
+
+        decideSnakeMove: function(snake) {
+            var move = snake.nextPosition();
+
+            if(move.x >= 0 && move.x < this.m && move.y >= 0 && move.y < this.n) {
+                switch(this.matrix[move.x][move.y]){
+                case this.cell.free:
+                    snake.move();
+                    break;
+                case this.cell.food:
+                    snake.feed();
+                    snake.move();
+                    this.generateFood();
+                    break;
+                case this.cell.snake:
+                    break;
+                }
+            }
+        },
+
+        generateFood: function() {
+            var freeCell = findRandomFreeCell.bind(this)();
+            this.matrix[freeCell.x][freeCell.y] = this.cell.food;
         }
     };
 })();
@@ -74,26 +110,34 @@ var Snake = (function() {
                         left:  { dx:  0,  dy: -1 },
                         right: { dx:  0,  dy:  1 }};
 
-    function nextPosition(position, direction) {
-        var x = position.x + directions[direction].dx,
-            y = position.y + directions[direction].dy;
-
-        return { x: x, y: y };
-    }
 
     return {
         directions: directions,
 
+        nextPosition: function() {
+            var x = this.body[0].x + directions[this.direction].dx,
+                y = this.body[0].y + directions[this.direction].dy;
+
+            return { x: x, y: y };
+        },
+
         move: function() {
-            var nextHead = nextPosition(this.head(), this.direction);
-            this.body.unshift(nextHead);
-            this.body.pop();
+            this.body.unshift(this.nextPosition());
+            if(this.stomach === 0) {
+                this.body.pop();
+            }
+            else {
+                this.stomach--;
+            }
         },
 
         create: function(x,y, direction) {
             var self = Object.create(this);
-            self.body = [{ x: x, y: y }];
+
+            self.body      = [{ x: x, y: y }];
             self.direction = direction;
+            self.stomach   = 0;
+
             return self;
         },
 
@@ -103,6 +147,10 @@ var Snake = (function() {
 
         bodyParts: function() {
             return this.body;
+        },
+
+        feed: function() {
+            this.stomach =+ 1;;
         }
     };
 })();
--- snake_init.js
+++ snake_init.js
@@ -1,13 +1,28 @@
 function start() {
-    var snake = Snake.create(20,20, 'right');
-    var board = Board.create(42, 42, [snake]);
+    var directions = {
+        38: 'up',
+        40: 'down',
+        37: 'left',
+        39: 'right'
+    };
 
-    UI.drawBoard(42,42);
+    var snake = Snake.create(20,20, 'right');
+    var board = Board.create(50, 50, [snake]);
+    board.generateFood();
+    UI.drawBoard(50,50);
 
     setInterval(function() {
         UI.updateBoard(board.matrix);
         board.moveSnakes();
-    }, 500);
+    }, 100);
+
+    document.onkeydown = function(e) {
+        console.log(_.keys(directions), (e.keyCode).toString());
+        if(_.contains(_.keys(directions), (e.keyCode).toString())) {
+            snake.direction = directions[e.keyCode];
+            console.log(snake);
+        }
+    };
 }
 
 start();
