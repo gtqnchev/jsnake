9ffbd429acf477475651ec948da095ebed5e8441
George <gtqnchev@gmail.com>
2015-02-08 10:26:21 +0200

    Add user input buffer

--- game.js
+++ game.js
@@ -1,11 +1,12 @@
 var Game = {
-    create: function(ui, board_x, board_y, snake_x, snake_y, snake_direction) {
+    create: function(ui, board_x, board_y, snake_x, snake_y, snake_direction, input_buffer) {
         var self = Object.create(this);
 
         self.ui = ui;
         self.snake = Snake.create(snake_x, snake_y, snake_direction);
         self.board = Board.create(board_x, board_y, [self.snake]);
         self.board.generateFood();
+        self.input_buffer = input_buffer;
 
         ui.initialize(board_x, board_y);
 
@@ -15,7 +16,10 @@ var Game = {
     loop: function() {
         if(!this.board.end) {
             this.ui.render(this.board.matrix);
-
+            if (this.input_buffer.length > 0) {
+                var move = this.input_buffer.shift();
+                this.snake.changeDirection(move);
+            }
             this.board.moveSnakes();
             setTimeout(this.loop.bind(this), 100);
         }
--- game_initializer.js
+++ game_initializer.js
@@ -1,9 +1,10 @@
 function startGame(board_length_x, board_length_y, snake_pos_x, snake_pos_y, snake_dir) {
-    var game = Game.create(UI, board_length_x, board_length_y, snake_pos_x, snake_pos_y, snake_dir);
+    var input_buffer = [];
+    var game = Game.create(UI, board_length_x, board_length_y, snake_pos_x, snake_pos_y, snake_dir, input_buffer);
 
     document.onkeydown = function(e) {
         if(_.contains(_.keys(UI.directions), (e.keyCode).toString())) {
-            game.snake.changeDirection(UI.directions[e.keyCode]);
+                input_buffer.push(UI.directions[e.keyCode]);
         }
     };
 
